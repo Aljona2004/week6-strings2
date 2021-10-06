71b0ee1c907d1d5349a8eb9865b04357c8146798
Opilane TTHK <opilane@tthk.ee>
2021-10-06 10:05:30 +0300

    morestrings

---
+++ ACharCounter/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ACharCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajal sisestada eesnime
+            //programm küsib kasurajal sisestada perekonnanime
+            //programm kuvab, mitu 'a'tähte kasutaja nimes ja
+            //perekonnanimes kokku on
+
+            Console.WriteLine("Sisesta eesnimi:");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Sisesta perekonnanimi:");
+            string lastName = Console.ReadLine();
+
+            string fullName = $"{firstName}{lastName}".ToLower();
+
+            
+            int counter = 0;
+
+            for(int i = 0; i < fullName.Length; i++)
+            {
+                if(fullName[i] == 'a')
+                {
+                    counter++; //counter = counter + 1
+                }
+            }
+
+            if(counter == 1)
+            {
+                Console.WriteLine($"Sinu nimes on {counter} 'a' tähte.");
+            }
+            else
+            {
+                Console.WriteLine($"Sinu nimes on {counter} 'a' tähte.");
+            }
+        }
+    }
+}
---
+++ MoreStrings/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MoreStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutaja sisestda eesnime ja perekonnanime
+            //programm tervitab kasutajat kasutades tema initsiaale
+            //initsiaalid lõpevad punktiga
+
+            Console.WriteLine("Sisesta oma eesnimi:");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("sisesta oma perekonnanimi:");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine($"Tere, {firstName[0]}. {lastName[0]}.");
+
+        }
+    }
+}
